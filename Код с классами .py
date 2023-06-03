ba2a1f46067a3ece33dcdb2d4c5fc4992c18ad41
nova2k1 <123230451+nova2k1@users.noreply.github.com>
2023-06-03 12:56:13 +0300

    Код с классами .py

--- Код с классами .py
+++ Код с классами .py
@@ -10,28 +10,28 @@ class CustomException3(BaseException):
 class MyClass:
     def method1(self):
         try:
-            # код метода
+        
             if some_condition:
                 raise CustomException1("Ошибка метода 1")
-            # остальной код метода
+            
         except CustomException1 as e:
             print("Обработка исключения:", e)
 
     def method2(self):
         try:
-            # код метода
+            
             if some_condition:
                 raise CustomException2("Ошибка метода 2")
-            # остальной код метода
+            
         except CustomException2 as e:
             print("Обработка исключения:", e)
 
     def method3(self):
         try:
-            # код метода
+        
             if some_condition:
                 raise CustomException3("Ошибка метода 3")
-            # остальной код метода
+            
         except CustomException3 as e:
             print("Обработка исключения:", e)
 
@@ -43,10 +43,8 @@ class AnotherClass:
             obj.method2()
             obj.method3()
         except BaseException as e:
-            print("Обработка исключения:", e)
-
-
-# Пример использования
+            print("Обработка исключения:", 
+            
 obj = MyClass()
 another_obj = AnotherClass()
 another_obj.process_object(obj)
